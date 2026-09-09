e176f5f3ef8a8b99c76f3ca4e01ec87b668e903e
KINDA Steve Regis <stevekinda71@gmail.com>
2026-09-09 12:50:25 +0000

    j'ai modifié ressource.java

--- ressources.java
+++ ressources.java
@@ -1,1 +1,22 @@
+public class ressources{
+    int numero;
+    String nomRess;
+    boolean dispo;
+    public ressources( int numero,String nomRess,boolean dispo){
+        this.numero=numero;
+        this.nomRess=nomRess;
+        this.dispo=true;
 
+    }
+    public void disponibilite(boolean dispo){
+        if (dispo=true){
+            System.out.println("le materiel est disponible");
+        }
+        else{
+            System.out.println("le materiel est disponible");
+        }
+    }
+    public void afficheRessource(){
+        System.out.println(" le nom du materiel est "+nomRess+" son numero est "+numero);
+    }
+}
