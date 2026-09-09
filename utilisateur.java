8ace0294cd3e606a70e0187fc6735b5355f788cb
Alim-vsc <dawoudalim@gmail.com>
2026-09-09 12:44:05 +0000

    j ai modifier le utilisateur.java

--- utilisateur.java
+++ utilisateur.java
@@ -1,6 +1,12 @@
-public class utilisateur{
-    int codeid;
+public class utilisateur{int codeID;
     String nom;
     String email;
-
+    public Utilisateur(int codeID,String nom,String email) {
+        this.codeID = codeID;
+        this.nom=nom;
+        this.email=email;
+    }
+    public void afficherUtilisateur(){
+        System.out.println("le nom est "+nom+" l identifiant est "+codeID+" l email est "+email);
+    }
 }
