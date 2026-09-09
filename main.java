2119f346bd7cde71e759c06f7d18e21f6838d63e
Alim-vsc <dawoudalim@gmail.com>
2026-09-09 13:18:01 +0000

    j ai modifie main.java et reservation.java

--- main.java
+++ main.java
@@ -1,1 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        Utilisateur OMAR=new Utilisateur(2,"OMAR","omar@gmail");
+        OMAR.afficherUtilisateur();
+        ressources videoprojecteur=new ressources(1,"videoprojecteur",true);
+        videoprojecteur.afficheRessource();
+        ressources  ordinateur =new ressources(2,"ordinateur",true);
+        ordinateur.afficheRessource();
+        reservation reservation1=new reservation();
+        reservation1.reserver(2,"OMAR","videoprojecteur",true,"10h","12h");
+        reservation1.annulation(true);
+
+
+    }
+}
 
--- reservation.java
+++ reservation.java
@@ -1,1 +1,21 @@
-
+public class reservation{
+    public Utilisateur codeID;
+    public Utilisateur nom;
+    public ressources nomRess;
+    public ressources numero;
+    public ressources dispo;
+    String heurdebut;
+    String heurfin;
+    public void reserver(int codeID,String nom,String nomRess,boolean dispo,String heurdebut,String heurfin){
+        if (dispo=true){
+            dispo=false;
+            System.out.println("l utilisateur "+nom+" d identifiant "+codeID+" a reserve le materiel "+nomRess+" de numero "+numero+" de "+heurdebut+" a "+heurfin);
+        }
+        else{
+             System.out.println("deja reserve");
+        }
+    }
+    public void annulation(boolean dispo){
+        dispo=true;
+    }
+}
