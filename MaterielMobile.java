96ac8acf20624aee6260a41027d7a55faa21919f
KINDA Steve Regis <stevekinda71@gmail.com>
2026-09-11 21:38:23 +0000

    initialisation de la classe MaterielMobile

--- MaterielMobile.java
+++ MaterielMobile.java
@@ -1,1 +1,18 @@
-
+public class materiel extends ressources{
+    String categorie;
+    public materiel(int numero,String nom,String categorie){
+        super( numero,nom);
+        this.categorie=categorie;
+    }
+    public String getcategorie(){
+        return categorie;
+    }
+    @Override
+    public String toString(){
+       super.toString();
+       return("lacategorie est "+categorie);
+    }
+    @Override
+    public int dureeMaxReservation(){
+        return 8;
+    }
