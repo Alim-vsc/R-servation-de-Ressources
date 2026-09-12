0b25a4fbf2afdedeba34d36acb52e3ebabd6a773
KINDA Steve Regis <stevekinda71@gmail.com>
2026-09-12 12:36:20 +0000

    modification

--- main.java
+++ main.java
@@ -1,16 +1,21 @@
 public class main {
     public static void main(String[] args) {
-        utilisateur OMAR=new utilisateur(2,"OMAR","omar@gmail");
-        OMAR.afficherUtilisateur();
-        ressources videoprojecteur=new ressources(1,"videoprojecteur",true);
-        videoprojecteur.afficheRessource();
-        ressources  ordinateur =new ressources(2,"ordinateur",true);
-        ordinateur.afficheRessource();
-        reservation reservation1=new reservation();
-        reservation1.reserver(2,"OMAR","videoprojecteur",true,"10h","12h");
-        reservation1.annulation(true);
-
-
+        ressources salleA=new salle(10,"salleA",40);
+        ressources projecteurA=new MaterielMobile(15,"projecteur02","videoProjecteur");
+        ressources pc=new MaterielMoblie(20,"pc01","ordinateur portable");
+        System.out.println(pc.toString());
+        System.out. println(projecteurA.tostring());
+        System.out.println(pc.dureeMaxReservation());
+        salleA.reserve();
+        salleA.dureeMaxReservation();
+        pc.dureeMaxReservation();
+        projecteurA.dureeMaxReservation();
+        System.out.println(salleA.getdisponible());
+        System.out.println(pc.getdisponible());
+        System.out.println(projecteurA.getdisponible());
+        salleA.liberer();
+        System.out.print(pc.getdisponible());
     }
 }
+
 
